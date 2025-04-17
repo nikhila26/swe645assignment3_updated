cdc0c0deb632e05a528ae4670f3d3f01665abbf4
nikhila26 <49489920+nikhila26@users.noreply.github.com>
2025-04-17 02:23:21 -0400

    Update SurveyController.java

--- src/main/java/com/example/controller/SurveyController.java
+++ src/main/java/com/example/controller/SurveyController.java
@@ -20,7 +20,7 @@ public class SurveyController {
     // Root endpoint for the application
 	@GetMapping("/")
 	public String home() {
-		return "Welcome to the Student Management Application! Please use this link to view the application. the database has 7 entries.";
+		return "Welcome to the Student Management Application! Please use this link to view the application. the database has few entries. please check";
 	}
 
 	// Endpoint to get all surveys
